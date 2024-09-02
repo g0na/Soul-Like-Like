6b0a9dfe0ac04edca05b0a77a2634aa908b4b872
g0na <dlcndgh70@gmail.com>
2024-09-03 04:27:48 +0900

    Jump 작업

--- Soul-Like-Like/Assets/PlayerController.cs
+++ Soul-Like-Like/Assets/PlayerController.cs
@@ -1,11 +1,16 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using Quaternion = UnityEngine.Quaternion;
+using Vector3 = UnityEngine.Vector3;
 
 public class PlayerController : MonoBehaviour
 {
     public float moveSpeed = 5f;
     public float turnSpeed = 5f;
+
+    private bool isGrounded;
+    public float groundCheckDistance = 0.1f;
+    public float jumpForce = 5f;
+    public LayerMask ground;
     private Vector3 movement;
     
     Rigidbody rb;
@@ -16,19 +21,23 @@ public class PlayerController : MonoBehaviour
     {
         rb = GetComponent<Rigidbody>();
         anim = GetComponent<Animator>();
-        
     }
 
     // Update is called once per frame
     void Update()
     {
+        GroundCheck();
         Move();
+        Jump();
+        
+        Debug.DrawRay(transform.position, Vector3.down * groundCheckDistance, Color.red);
+        Debug.Log(isGrounded);
     }
 
     void Move()
     {
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
 
         // 벡터의 정규화를 통해서 모든 방향의 이동속도를 동일하게 만든다.
         movement = new Vector3(horizontal, 0, vertical).normalized;
@@ -37,13 +46,37 @@ public class PlayerController : MonoBehaviour
         transform.position += movement * moveSpeed * Time.deltaTime;
         
         // 애니메이션
-        anim.SetBool("Walking", movement != Vector3.zero);
+        anim.SetBool("Running", movement != Vector3.zero);
         
         // 회전
         if (movement != Vector3.zero) // 키 입력값이 존재할 때
         {
             Quaternion toRotation = Quaternion.LookRotation(movement, Vector3.up); // 해당 방향을 바라봄
             transform.rotation = Quaternion.Lerp(transform.rotation, toRotation, turnSpeed * Time.deltaTime); // 부드러운 회전
+        }
+    }
+
+    void Jump()
+    {
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            Debug.Log("jump!~");
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse); // Add an instant force impulse to the rigidbody, using its mass.
+            anim.SetBool("Jumping", isGrounded);
+        }
+    }
+    
+    void GroundCheck()
+    {
+        
+        // 플레이어의 위치에서, 아래방향으로, groundDistance 만큼 ray를 쏴서, ground 레이어가 있는지 검사
+        if (Physics.Raycast(transform.position, Vector3.down, groundCheckDistance, ground))
+        {
+            isGrounded = true;
+        }
+        else
+        {
+            isGrounded = false;
         }
     }
 }
