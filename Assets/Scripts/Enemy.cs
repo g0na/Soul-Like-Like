e9b2e90a8b89d7008a77f45d8fe40be255fbd346
op4804 <op4804@gmail.com>
2025-04-01 23:09:07 +0900

    working on Enemy Attack

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -9,10 +9,22 @@ public class Enemy : MonoBehaviour
 
     public SphereCollider enemyCognitionRange;
 
+    private float moveSpeed = 0.4f;
+
+    bool isPlayerCloseEnough = false;
+    float attackRange = 1.1f;
+    public float distanceToPlayer;
+
+    Animator anim;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
 
     private void Update()
     {
-
+        Attack();
     }
 
     private void OnTriggerEnter(Collider other)
@@ -20,14 +32,43 @@ public class Enemy : MonoBehaviour
         if (other.CompareTag("Player"))
         {
             Debug.Log("Player Detected");
+            isPlayerCloseEnough = true;
+            StartCoroutine(Follow());
+            anim.SetBool("walk", true);
             // TODO: Attack Player
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Player Out of Range");
+            isPlayerCloseEnough = false;
+        }
+    }
+
+    private void Attack()
+    {
+        if (isPlayerCloseEnough && distanceToPlayer <= attackRange)
+        {
+            anim.SetTrigger("attack");
+        }
+    }
+
+    IEnumerator Follow()
+    {
+        while (isPlayerCloseEnough)
+        {
+            this.transform.LookAt(GameManager.Instance.player.transform);
+            this.transform.position += this.transform.forward * moveSpeed * Time.deltaTime;
+            distanceToPlayer = Vector3.Distance(transform.position, GameManager.Instance.player.transform.position);
+            yield return new WaitForFixedUpdate();
         }
     }
 
     public void Hit(int dmg)
     {
-
-
         this.hp -= dmg;
         UIManager.Instance.ShowDamageText(dmg);
         if (hp < 0)
